57cab92d5fff2386e961dd9e246db72188096a28
bishboy <bishboy2spieking@gmail.com>
2025-09-18 16:20:00 +0100

    update

--- src/component/Home/CategoryCard.jsx
+++ src/component/Home/CategoryCard.jsx
@@ -3,7 +3,6 @@ import { categories } from '@/data';
 import { Card, CardContent, CardFooter } from '@/components/ui/card';
 import { Link } from 'react-router';
 import { CATEGORY_URL } from '@/routes/constant/urlConstant';
- 
 
 const CategoryCard = () => {
     return (
@@ -15,18 +14,19 @@ const CategoryCard = () => {
                     </h2>
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
                         {categories.map((category, index) => (
-                            <Link to={CATEGORY_URL} >
                             <Card
                                 key={index}
                                 className="card-wavy bg-white border border-gray-200 rounded-xl py-12 flex flex-col items-center text-center shadow-xl hover:shadow-2xl transition-all duration-200 group"
                             >
+                                <Link
+                                    to={CATEGORY_URL}
+                                    className="w-full flex flex-col items-center z-10 flex-grow"
+                                >
+                                    <div className="mb-4 p-4 bg-gray-50 rounded-md">
+                                        <img src={category.icon} alt="" className="h-12 w-12 text-indigo-600" />
+                                    </div>
 
-                                <div className="mb-4  p-4 bg-gray-50 rounded-md">
-                                    <img src={category.icon} alt="" className="h-12 w-12 text-indigo-600" />
-                                    {/* <category.icon className="h-10 w-10 text-indigo-600" /> */}
-                                </div>
- 
-                                    <CardContent className="p-0 flex-grow">
+                                    <CardContent className="p-0 flex-grow w-full">
                                         <h3 className="text-xl font-semibold group-hover:text-indigo-600 text-gray-800 mb-2">
                                             {category.title}
                                         </h3>
@@ -38,15 +38,12 @@ const CategoryCard = () => {
                                             <span className=''>{category.articles} articles</span>
                                         </div>
                                     </CardFooter>
-                               
+                                </Link>
                             </Card>
-                            
-                            </Link>
                         ))}
                     </div>
                 </div>
             </div>
-
         </div>
     )
 }
--- src/component/Layout.jsx
+++ src/component/Layout.jsx
@@ -3,10 +3,12 @@ import Footer from './Footer'
 import { Outlet } from 'react-router'
 import "../App.css"
 import "../component/Home/Waxy.css"
+import ScrollToTop from '@/ScrollToTop'
 
 const Layout = ({ children }) => {
   return (
     <div className=''>
+      <ScrollToTop />
       <main className='min-h-screen' >
         <Outlet />
       </main>
